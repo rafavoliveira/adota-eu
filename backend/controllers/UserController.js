51c0e17b21cd6221f657b6df91e8d8b4f8c0683e
Rafael Vasconcelos Oliveira <rafael.v.oliveira0806@gmail.com>
2022-06-01 09:11:20 -0300

    criação de validação de dados do usuário

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,6 +2,41 @@ const User = require("../models/User");
 
 module.exports = class UserController {
   static async register(req, res) {
-    res.json("Olá get a pet");
+    const { name, email, phone, password, confirmPassword } = req.body;
+
+    //validations
+    if (!name) {
+      res.status(422).json({ message: "O nome é obrigatório" });
+      return;
+    }
+    if (!email) {
+      res.status(422).json({ message: "O email é obrigatório" });
+      return;
+    }
+    if (!phone) {
+      res.status(422).json({ message: "O telefone é obrigatório" });
+      return;
+    }
+    if (!password) {
+      res.status(422).json({ message: "A senha é obrigatória" });
+      return;
+    }
+    if (!confirmPassword) {
+      res.status(422).json({ message: "Você precisa confirmar sua senha" });
+      return;
+    }
+    if (password !== confirmPassword) {
+      res.status(422).json({ message: "As senhas precisam ser iguais" });
+      return;
+    }
+    //check if user exists
+    const userExists = await User.findOne({ email: email });
+
+    if (userExists) {
+      res.status(422).json({
+        message: "Ops! Este email já está cadastrado",
+      });
+      return;
+    }
   }
 };
